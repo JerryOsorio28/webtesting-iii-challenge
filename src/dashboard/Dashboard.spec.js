a8805f54971b62900909571c8a39f17f3aee48f8
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-08-19 17:56:29 -0400

    Got dependencies properly installed and working

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
-// Test away
+import React from 'react';
+
+//Import renderer from react-test-renderer library
+import renderer from 'react-test-renderer';
+
+//import Dashboard component
+import Dashboard from './Dashboard';
+
+describe ('<Dashboard />', () => {
+    it('should match snapshot', () => {
+        const tree = renderer.create(<Dashboard />).toJSON();
+
+        expect(true).toMatchSnapshot();
+    })
+})
