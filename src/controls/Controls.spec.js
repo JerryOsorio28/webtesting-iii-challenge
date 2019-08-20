5de99af61c6f08f412b04ec65ef03892bc3f9ebb
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-08-19 21:17:59 -0400

    WIP for web testing project

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -7,7 +7,7 @@ import renderer from 'react-test-renderer';
 import Controls from './Controls';
 
 //import render from react-testing-library
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 describe ('<Controls />', () => {
     it('should match snapshot', () => {
@@ -15,12 +15,58 @@ describe ('<Controls />', () => {
 
         expect(true).toMatchSnapshot();
     });
-    it('button should lock when clicked', () => {
-        const { getByText } = render(<Controls locked={ true } closed={ false }/>);
+
+    // it('button should lock when clicked', () => {
+    //     const { getByText } = render(<Controls locked={ true } closed={ false }/>);
+
+    //     fireEvent.click(getByText(/close gate/i));
+    //     expect(getByText(/lock gate/i));
+    //     expect(getByText(/open gate/i));
+    //   });
+
+    it('close gate', () => {
+        const { getByText } = render(<Controls closed={false} locked={false}/>);
+
+        const button = getByText(/close gate/i)
+
+        fireEvent.click(button);
         expect(getByText(/lock gate/i));
         expect(getByText(/close gate/i));
       });
+
+      it('lock gate', () => {
+          const { getByText } = render(<Controls closed={true} locked={false} />);
+
+          const button = getByText(/lock gate/i)
+          fireEvent.click(button);
+          expect(getByText(/lock gate/i));
+          expect(getByText(/open gate/i));
+        });
+        
+        it('unlock gate', () => {
+            const { getByText } = render(<Controls locked={true} closed={true}/>);
+
+            const button = getByText(/unlock gate/i)
+            
+            fireEvent.click(button);
+            expect(getByText(/unlock gate/i));
+            expect(getByText(/open gate/i));
+        });
+        
+        it('open gate', () => {
     
+            const { getByText } = render(<Controls closed={true} locked={false}/>);
+
+            const button = getByText(/open gate/i)
+    
+            fireEvent.click(button);
+            expect(getByText(/lock gate/i));
+            expect(getByText(/open gate/i));
+          });
+        
 });
+
+// const Controls = props => {
+//     const { locked, closed, toggleLocked, toggleClosed } = props;
 
 
